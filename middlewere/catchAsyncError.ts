16f798c9a4bab03f4153c7eef55e58f48e548e0a
Michael Mengistu <michaelmengistu12@gmail.com>
2024-08-12 16:02:02 +0300

    user controller

--- middlewere/catchAsyncError.ts
+++ middlewere/catchAsyncError.ts
@@ -3,3 +3,16 @@ import { NextFunction, Request, Response } from "express";
 export const CatchAsyncError=(theFunc:any)=>(req:Request,res:Response,next:NextFunction)=>{
     Promise.resolve(theFunc(req,res,next)).catch(next);
 };
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    avatar: {
+        public_id: string;
+        url: string;
+    };
+    role: string;
+    isVerified: boolean;
+    courses: Array<{ courseId: string; }>;
+    comparePassword: (password: string) => Promise<boolean>;
+}
