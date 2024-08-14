6eb51b3c57ba908cf6816f68235c458d1b0e74d1
Michael Mengistu <michaelmengistu12@gmail.com>
2024-08-14 10:28:32 +0300

    login user and jwt

--- models/user.model.ts
+++ models/user.model.ts
@@ -63,6 +63,15 @@ userSchema.pre<IUser>('save',async function(next){
     this.password = await bcrypt.hash(this.password,10)
     next();
 })
+//sign access token
+userSchema.methods.SignAccessToken=function(){
+return jwt.sign.({id:this._id},process.env.ACCESS_TOKEN || "")
+}
+//sign refresh token
+userSchema.methods.SignRefreshToken =function(){
+    return jwt.sign({id:this._id},process.env.REFERSH_TOKEN ||"")
+}
+
 // compare password 
 userSchema.methods.comparePassword =async function(enteredPassword:string):Promise<boolean>{return await bcrypt.compare(enteredPassword,this.password)
 
